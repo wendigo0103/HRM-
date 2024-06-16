7b795e2524635ddd7e639b46b207d97e60930324
Wendigo0103 <roshansamuel6312@gmail.com>
2024-06-16 13:39:18 -0700

    Added a few buttons and text boxes

---
+++ index.js
@@ -0,0 +1,4 @@
+        // Example JavaScript for interactivity
+        document.querySelector('button').addEventListener('click', function () {
+            alert('Submit button clicked!');
+        });
